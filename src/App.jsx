741cb2be32e99fbe6016a26305522e2942ace9e3
HieuITMHG <danghuuhieu038@gmail.com>
2026-03-29 19:31:17 +0700

    okla

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './components/login';
+import Login from './components/Login';
 import Chat from './components/Chat';
 
 function App() {
